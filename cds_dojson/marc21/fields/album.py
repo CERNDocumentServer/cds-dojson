af37541ea1c020cac69eb2d18059cdb57e7e0263
Orestis Melkonian <melkon.or@gmail.com>
2016-11-29 10:35:18 +0100

    marc21: fields update
     * Update MARC21 fields due to the latest Library of Congress standard.
    
    Signed-off-by: Orestis Melkonian <melkon.or@gmail.com>

--- cds_dojson/marc21/fields/album.py
+++ cds_dojson/marc21/fields/album.py
@@ -24,7 +24,7 @@ from dojson import utils
 from cds_dojson.marc21.models.album import model as marc21
 
 
-@marc21.over('images', '^774[_01][_8]', override=True)
+@marc21.over('images', '^774[_10][_8]', override=True)
 @utils.for_each_value
 @utils.filter_values
 def images(self, key, value):

ba704019fe182af31c57b3c3fb204adca0a54427
Leonardo Rossi <leonardo.r@cern.ch>
2017-07-25 17:27:00 +0200

    marc21: translate 8567_ fields
    * Translates 8567_ fields. (addresses #85)
    
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- cds_dojson/marc21/models/videos/video.py
+++ cds_dojson/marc21/models/videos/video.py
@@ -55,13 +55,9 @@ class CDSVideo(OverdoJSONSchema):
         '700__0',
         '700__9',
         '773__o',
-        # FIXME can I ignore?
-        '8567_2',
         '916__s',
         '916__w',
         '937__c',
-        # FIXME remove from ignored!
-        '937__s',
         '960__a',
         '961__c',
         '961__h',

f3664d96d29a0e0df6739bc46289c5503210dba8
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-06-30 10:50:19 +0200

    migration: updated schema changes
    * changed all the identifiers to common structure
    * removed all the emails
    * removed funding info
    * restructurised title objects

--- cds_dojson/marc21/fields/books/book.py
+++ cds_dojson/marc21/fields/books/book.py
@@ -27,39 +27,51 @@ from cds_dojson.marc21.fields.books.errors import ManualMigrationRequired, \
     MissingRequiredField, UnexpectedValue
 from cds_dojson.marc21.fields.books.utils import is_excluded, extract_parts, \
     extract_volume_number
-from cds_dojson.marc21.fields.utils import clean_val
+from cds_dojson.marc21.fields.utils import clean_val, filter_list_values, \
+    out_strip
 from cds_dojson.marc21.models.books.book import model
+from .base import alternative_titles as alternative_titles_base
 
 
-@model.over('alternative_titles', '^246__')
-@for_each_value
-@filter_values
+@model.over('alternative_titles', '(^246__)|(^242__)')
+@filter_list_values
 def alternative_titles(self, key, value):
     """Alternative titles."""
-    if ('n' in value and 'p' not in value) or \
-       ('n' not in value and 'p' in value):
-        raise MissingRequiredField(subfield='n or p')
+    _alternative_titles = self.get('alternative_titles', [])
 
-    if 'p' in value:
-        _migration = self.get('_migration', {})
-        if 'volumes' not in _migration:
-            _migration['volumes'] = []
+    if key == '242__':
+        _alternative_titles += (alternative_titles_base(self, key, value))
+    elif key == '246__':
+        if ('n' in value and 'p' not in value) or \
+           ('n' not in value and 'p' in value):
+            raise MissingRequiredField(subfield='n or p')
 
-        val_n = clean_val('n', value, str)
-        _migration['volumes'].append({
-            'volume': extract_volume_number(val_n, raise_exception=True),
-            'title': clean_val('p', value, str),
-        })
-        _migration['is_multipart'] = True
-        _migration['record_type'] = 'multipart'
-        self['_migration'] = _migration
-        raise IgnoreKey('alternative_titles')
-    else:
-        return {
-            'title': clean_val('a', value, str, req=True),
-            'subtitle': clean_val('b', value, str),
-            'source': clean_val('i', value, str),
-        }
+        if 'p' in value:
+            _migration = self.get('_migration', {})
+            if 'volumes' not in _migration:
+                _migration['volumes'] = []
+
+            val_n = clean_val('n', value, str)
+            _migration['volumes'].append({
+                'volume': extract_volume_number(val_n, raise_exception=True),
+                'title': clean_val('p', value, str),
+            })
+            _migration['is_multipart'] = True
+            _migration['record_type'] = 'multipart'
+            self['_migration'] = _migration
+            raise IgnoreKey('alternative_titles')
+        else:
+            if 'a' in value:
+                _alternative_titles.append({
+                    'value': clean_val('a', value, str, req=True),
+                    'type': 'ALTERNATIVE_TITLE',
+                })
+            if 'b' in value:
+                _alternative_titles.append({
+                    'value': clean_val('b', value, str, req=True),
+                    'type': 'SUBTITLE',
+                })
+            return _alternative_titles
 
 
 @model.over('number_of_pages', '^300__')   # item
@@ -72,21 +84,23 @@ def number_of_pages(self, key, value):
     parts = extract_parts(val)
     if parts['has_extra']:
         raise UnexpectedValue(subfield='a')
-    if parts['physical_description']:
-        self['physical_description'] = parts['physical_description']
+    if parts['physical_copy_description']:
+        self['physical_copy_description'] = parts['physical_copy_description']
     if parts['number_of_pages']:
         return parts['number_of_pages']
     raise UnexpectedValue(subfield='a')
 
 
 @model.over('title', '^245__')
-@filter_values
+@out_strip
 def title(self, key, value):
     """Translates title."""
     if 'title' in self:
         raise UnexpectedValue()
 
-    return {
-        'title': clean_val('a', value, str, req=True),
-        'subtitle': clean_val('b', value, str),
-    }
+    if 'b' in value:
+        _alternative_titles = self.get('alternative_titles', [])
+        _alternative_titles.append(
+            {'value': clean_val('b', value, str), 'type': 'SUBTITLE'})
+        self['alternative_titles'] = _alternative_titles
+    return clean_val('a', value, str, req=True)

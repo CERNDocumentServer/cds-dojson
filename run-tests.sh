9f51c3156855f0a4d499df38e20a87fb8e692048
jrcastro2 <jrcastro9515@gmail.com>
2020-07-30 18:01:47 +0200

    document: Adds extension fields rules
    * Adds rule to translate the
      standard_status field
    * closes #257

--- run-tests.sh
+++ run-tests.sh
@@ -18,6 +18,6 @@
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 pydocstyle cds_dojson tests && \
-isort --check-only --diff && \
+isort . --check-only --diff && \
 check-manifest --ignore ".travis-*" && \
 python setup.py test
